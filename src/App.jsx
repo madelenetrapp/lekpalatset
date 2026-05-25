b5302e11082750899bb8d5cd0833dd98a2eff866
Madelene <madelenetrapp@outlook.com>
2026-05-25 18:05:26 +0200

    fixade kundvagn borttagning

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,4 @@ function App() {
 export default App
 
 
-// Förklaring till varför jag väljer denna lösning: jag valde routes och route eftersom min app är ganska lite och jag ville ha en mer lättläst struktur. createHashRouter är mer anvancerad och används ofta i större projekt eftersom man kan hantera gemensama layout och nestlad layoute (flera sidor delar samma grundlayoute) innan sidan visas. Med children och outlet kan flera sidor dela samma layout (ex header och footer). I mitt projekt känns det som jag vill behålla routes och route (efter feedbacken vid kod genomgång) eftersom Zustand redan hanterar state och datahämtning. Om projektet blir större kommer jag ha behov av de extra funktionerna som createHashRouter har. 
-
 
